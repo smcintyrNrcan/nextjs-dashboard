66c68e6ff8bd17b1ba9d54b235038e35304c10fe
Scott McIntyre <scott.mcintyre@nrcan-rncan.gc.ca>
2024-08-29 15:40:35 -0600

    Improving Accessibility 1

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from "@/app/lib/actions";
+import { updateInvoice, State} from "@/app/lib/actions";
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -20,6 +21,10 @@ export default function EditInvoiceForm({
 }) {
 
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
+  const initialState: State = { message: null, errors: {} };
+  //const [state, formAction] = useActionState(updateInvoice, initialState);
+  
 
   return (
     <form action={updateInvoiceWithId}>
